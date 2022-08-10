e587aaddf024b0446972294bc91a4111542b1d2d
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-08-10 15:25:54 -0400

    setup project

---
+++ AppWithPlugin/Program.cs
@@ -0,0 +1,47 @@
+﻿using PluginBase;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AppWithPlugin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1 && args[0] == "/d")
+                {
+                    Console.WriteLine("Waiting for any key...");
+                    Console.ReadLine();
+                }
+
+                // Load commands from plugins.
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Commands: ");
+                    // Output the loaded commands.
+                }
+                else
+                {
+                    foreach (string commandName in args)
+                    {
+                        Console.WriteLine($"-- {commandName} --");
+
+                        // Execute the command with the name passed as an argument.
+
+                        Console.WriteLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
