da525c7dbd53a4339c7e8ed26cd725a20a10b48e
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-08-10 14:40:59 -0400

    project setup

---
+++ TTS_Banking_Console_App/Program.cs
@@ -0,0 +1,4 @@
+﻿using TTS_Banking_Console_App;
+
+var account = new BankAccount("Jay", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with an initial balance of ${account.Balance}");
