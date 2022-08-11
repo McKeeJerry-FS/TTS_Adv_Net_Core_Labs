75c7a5b14f01828f97e6a9e5f70a14c1f56fca2a
Jerry McKee Jr <jmckee0882@student.fullsail.edu>
2022-08-10 20:12:11 -0400

    created project

--- AppWithPlugin/Program.cs
+++ AppWithPlugin/Program.cs
@@ -1,8 +1,4 @@
 ﻿using PluginBase;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace AppWithPlugin
@@ -19,12 +15,24 @@ namespace AppWithPlugin
                     Console.ReadLine();
                 }
 
-                // Load commands from plugins.
+                string[] pluginPaths = new string[]
+                {
+                    // Paths to plugins to load.
+                };
+
+                IEnumerable<ICommand> commands = pluginPaths.SelectMany(pluginPath =>
+                {
+                    Assembly pluginAssembly = LoadPlugin(pluginPath);
+                    return CreateCommands(pluginAssembly);
+                }).ToList();
 
                 if (args.Length == 0)
                 {
                     Console.WriteLine("Commands: ");
-                    // Output the loaded commands.
+                    foreach (ICommand command in commands)
+                    {
+                        Console.WriteLine($"{command.Name}\t - {command.Description}");
+                    }
                 }
                 else
                 {
@@ -41,6 +49,56 @@ namespace AppWithPlugin
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
+            }
+        }
+
+        static Assembly LoadPlugin(string relativePath)
+        {
+            // Navigate up to the solution root
+            string root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(
+                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
+
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            Console.WriteLine($"Loading commands from: {pluginLocation}");
+            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
+
+        private class PluginLoadContext
+        {
+            public PluginLoadContext(string pluginLocation)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        static IEnumerable<ICommand> CreateCommands(Assembly assembly)
+        {
+            int count = 0;
+
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(ICommand).IsAssignableFrom(type))
+                {
+                    ICommand result = Activator.CreateInstance(type) as ICommand;
+                    if (result != null)
+                    {
+                        count++;
+                        yield return result;
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                string availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                throw new ApplicationException(
+                    $"Can't find any type which implements ICommand in {assembly} from {assembly.Location}.\n" +
+                    $"Available types: {availableTypes}");
             }
         }
     }
